a5c452b86fdf255b9b389278ddc3310c07b50b29
Pat Cavit <github@patcavit.com>
2018-08-04 23:28:47 -0700

    chore: setting up shop

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = {
+    extends : "@tivac",
+    
+    parserOptions : {
+        ecmaVersion : 6,
+        sourceType  : "module",
+    },
+
+    env : {
+        es6  : true,
+        node : true,
+        jest : true,
+    },
+};
---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const pkg = require("./package.json");
+
+const input = "./src/router.js";
+
+module.exports = [
+    // ESM & CJS builds
+    {
+        input,
+
+        output : [{
+            file      : pkg.main,
+            format    : "cjs",
+            sourcemap : true,
+        }, {
+            file      : pkg.module,
+            format    : "es",
+            sourcemap : true,
+        }],
+    },
+
+    // browser UMD build
+    {
+        input,
+
+        output : {
+            name      : pkg.name,
+            file      : pkg.browser,
+            format    : "umd",
+            sourcemap : true,
+        },
+
+        plugins : [
+            require("rollup-plugin-terser")(),
+            require("rollup-plugin-buble"),
+        ],
+    },
+];
