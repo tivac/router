0c343b3f1d81aadfd28ddafc25c65c93643795ca
Pat Cavit <github@patcavit.com>
2018-10-17 21:19:03 -0700

    test: remove now-unnecessary hack

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,9 @@
 module.exports = {
     projects : [
         {
-            displayName : "hrouter tests",
+            displayName : "test",
 
             watchPathIgnorePatterns : [ "<rootDir>/src/" ],
-            
-            // Fix exciting localStorage issues because jest uses jsdom wrong
-            // https://github.com/jsdom/jsdom/issues/2304
-            // https://github.com/facebook/jest/pull/6792
-            testURL : "http://localhost",
         },
         {
             runner      : "jest-runner-eslint",
